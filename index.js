b33d883e8e3a72a392ae2f205948c55243aa0f3a
j-alipate <j.alipate@gmail.com>
2020-07-29 22:01:42 +1200

    created a root route that serves a message

--- index.js
+++ index.js
@@ -6,4 +6,7 @@ server.listen(port, () => {
     console.log('listening on port ' + port)
 })
 
-server.use(express.static('public'))
+// server.use(express.static('public'))
+server.get('/', (req, res) => {
+    res.send('<h1>Yo!</h2>')
+})
