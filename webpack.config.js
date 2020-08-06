6ad45b87e60ab28fabe6e5e293f493a3a7786f70
Jonathan Alipate <j.alipate@gmail.com>
2020-08-07 10:24:47 +1200

    created webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+{
+    "mode": "development",
+        "entry": "server/index.js",
+            "output": {
+        "path": __dirname + '/server',
+            "filename": "[name].[chunkhash:8].js"
+    },
+    "module": {
+        "rules": [
+            {
+                "enforce": "pre",
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": "eslint-loader"
+            },
+            {
+                "test": /\.(js|jsx)$/,
+                "exclude": /node_modules/,
+                "use": {
+                    "loader": "babel-loader",
+                    "options": {
+                        "presets": [
+                            "env",
+                            "react"
+                        ]
+                    }
+                }
+            }
+        ]
+    }
+}
