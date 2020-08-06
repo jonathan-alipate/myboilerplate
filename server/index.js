44db13cb3dbf7b1258fa699fe1878c63da2bfd7d
Jonathan Alipate <j.alipate@gmail.com>
2020-08-07 09:32:19 +1200

    hooked up nodemon

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const server = express()
 const port = 3000
 
 server.listen(port, () => {
-    console.log('listening on port ' + port)
+    console.log('Server started: listening on port ' + port)
 })
 
 server.use(express.static('server/public'))
