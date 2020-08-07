9d6b715cae0b875a5609dd96ac167012d609be8e
Jonathan Alipate <j.alipate@gmail.com>
2020-08-08 09:11:28 +1200

    updated package JSON

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,12 @@
-{
-    "mode": "development",
-        "entry": "server/index.js",
-            "output": {
-        "path": __dirname + '/server',
-            "filename": "[name].[chunkhash:8].js"
+module.exports = {
+    "mode": "production",
+    "entry": "client/index.js",
+    "output": {
+        "path": __dirname + 'server/public',
+        "filename": "bundle.js"
     },
     "module": {
         "rules": [
-            {
-                "enforce": "pre",
-                "test": /\.(js|jsx)$/,
-                "exclude": /node_modules/,
-                "use": "eslint-loader"
-            },
             {
                 "test": /\.(js|jsx)$/,
                 "exclude": /node_modules/,
